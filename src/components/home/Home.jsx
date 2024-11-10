4cd7fca81da8ae93e112f33c1ea954ca97ea4ef8
KevinPramudyanto <kevinpramudyanto92@gmail.com>
2024-11-10 21:20:24 +0800

    Add search logic in home page

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import HomeTitle from "./HomeTitle";
 import HomeSearch from "./HomeSearch";
 import HomeFilter from "./HomeFilter";
@@ -7,6 +8,9 @@ import useFetchRestaurants from "../../data/useFetchRestaurants.js";
 
 const Home = () => {
   const { restaurants, loading, error } = useFetchRestaurants();
+  const [search, setSearch] = useState("");
+
+  const handleSearch = (e) => setSearch(e.target.value);
 
   return (
     <>
@@ -14,7 +18,11 @@ const Home = () => {
         <HomeTitle />
 
         <div className="flex flex-col items-start justify-between gap-5 lg:flex-row-reverse lg:items-center">
-          <HomeSearch />
+          <HomeSearch
+            search={search}
+            handleSearch={handleSearch}
+            names={restaurants.map((restaurant) => restaurant.restaurant_name)}
+          />
           <HomeFilter />
         </div>
 
@@ -32,23 +40,29 @@ const Home = () => {
 
         {!loading && !error && (
           <div className="grid grid-cols-1 gap-8 lg:grid-cols-2">
-            {restaurants.map((restaurant, idx) => (
-              <HomeRestaurant
-                key={idx}
-                id={restaurant._id}
-                name={restaurant.restaurant_name}
-                img={restaurant.img}
-                address={restaurant.address}
-                time={restaurant.opening_hours}
-                adultPrice={restaurant.price_range_adult}
-                childPrice={restaurant.price_range_child}
-                description={restaurant.description}
-                cuisine={restaurant.cuisine}
-                website={restaurant.website}
-                rating={restaurant.rating} //to add
-                review={restaurant.review} //to add
-              />
-            ))}
+            {restaurants
+              .filter((restaurant) =>
+                restaurant.restaurant_name
+                  .toLowerCase()
+                  .includes(search.toLowerCase()),
+              )
+              .map((restaurant, idx) => (
+                <HomeRestaurant
+                  key={idx}
+                  id={restaurant._id}
+                  name={restaurant.restaurant_name}
+                  img={restaurant.img}
+                  address={restaurant.address}
+                  time={restaurant.opening_hours}
+                  adultPrice={restaurant.price_range_adult}
+                  childPrice={restaurant.price_range_child}
+                  description={restaurant.description}
+                  cuisine={restaurant.cuisine}
+                  website={restaurant.website}
+                  rating={restaurant.rating} //to add
+                  review={restaurant.review} //to add
+                />
+              ))}
           </div>
         )}
       </div>
--- src/components/home/HomeSearch.jsx
+++ src/components/home/HomeSearch.jsx
@@ -1,13 +1,33 @@
 import { FaSearch } from "react-icons/fa";
 
-const HomeSearch = () => {
+const HomeSearch = (props) => {
   return (
     <div className="flex items-center justify-between gap-2">
       <input
         className="w-40 rounded border border-gray-400 bg-transparent p-2 hover:border-white"
+        list="searches"
+        id="search"
+        type="search"
+        value={props.search}
+        onChange={props.handleSearch}
         placeholder="Search…"
       />
-      <FaSearch size={25} />
+
+      <datalist id="searches">
+        {props.names
+          .filter(
+            (name) =>
+              props.search.length >= 2 &&
+              name.toLowerCase().includes(props.search.toLowerCase()),
+          )
+          .map((name, idx) => (
+            <option key={idx} value={name} />
+          ))}
+      </datalist>
+
+      <label htmlFor="search" title="Search by restaurant name">
+        <FaSearch size={25} />
+      </label>
     </div>
   );
 };
