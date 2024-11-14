fc152452f7eda9bb5b57e8a13fadabbe08c9d7fc
eeexuanlyx <eeexuanlyx@gmail.com>
2024-11-14 18:17:08 +0800

    tidy codes

--- src/pages/Detail.jsx
+++ src/pages/Detail.jsx
@@ -37,7 +37,7 @@ const Detail = () => {
 
   return (
     <div className="m-auto mx-9 max-w-screen-xl text-left">
-      <div className="text-sm-custom">
+      <div className="text-sm">
         <div className="mx-auto max-w-4xl p-6">
           <div className="md:flex-column flex flex-col gap-6 rounded-lg border border-gray-200 bg-white p-6 shadow-sm">
             <div className="flex items-center justify-between border-b border-gray-200 pb-4 pr-4">
@@ -61,11 +61,11 @@ const Detail = () => {
         </div>
       </div>
 
-      <div className="text-sm-custom">
+      <div className="text-sm">
         <div className="mx-auto max-w-4xl p-6">
           <div className="flex flex-col gap-6 rounded-lg border border-gray-200 bg-white p-6 shadow-sm md:flex-row">
             <div className="flex-1 border-r border-gray-200 pr-4">
-              <h2 className="mb-2 text-sm-custom font-semibold">ABOUT</h2>
+              <h2 className="mb-2 text-sm font-semibold">ABOUT</h2>
 
               <div className="my-4 mb-2 border-t border-gray-300 leading-relaxed">
                 <p className="mt-4">
@@ -88,7 +88,7 @@ const Detail = () => {
             </div>
 
             <div className="flex-1 border-r border-gray-200 px-4">
-              <h2 className="mb-2 text-sm-custom font-semibold">DETAILS</h2>
+              <h2 className="mb-2 text-sm font-semibold">DETAILS</h2>
               <div className="my-4 border-t border-gray-300">
                 <div className="mt-4">
                   <strong>Opening Hours:</strong>
@@ -105,7 +105,7 @@ const Detail = () => {
                 {restaurant.cuisine.map((cuisine, index) => (
                   <span
                     key={index}
-                    className="mr-1 rounded-sm bg-blue-400 px-1 py-1 text-sm-custom2 text-white"
+                    className="mr-1 rounded-sm bg-blue-400 px-1 py-1 text-xs text-white"
                   >
                     {cuisine}
                   </span>
@@ -120,12 +120,10 @@ const Detail = () => {
             </div>
 
             <div className="flex-1 pl-4">
-              <h2 className="mb-2 text-sm-custom font-semibold">
-                RATINGS & REVIEWS
-              </h2>
+              <h2 className="mb-2 text-sm font-semibold">RATINGS & REVIEWS</h2>
               <div className="my-4 border-t border-gray-300">
                 <strong>
-                  <p className="mt-4 text-2xl-custom">4.5/5</p>
+                  <p className="text-text-base mt-4">4.5/5</p>
                 </strong>
               </div>
               <div className="flex flex-col items-center justify-center gap-1">
@@ -162,14 +160,14 @@ const Detail = () => {
           </div>
         </div>
 
-        <div className="text-sm-custom">
+        <div className="text-sm">
           <div className="mx-auto max-w-4xl p-6">
             <div className="md:flex-column flex flex-col gap-6 rounded-lg border border-gray-200 bg-white p-6 shadow-sm">
               <div
                 ref={reviewsRef}
                 className="flex-1 border-b border-gray-200 pb-4 pr-4"
               >
-                <h2 className="mb-2 text-sm-custom font-semibold">REVIEWS</h2>
+                <h2 className="mb-2 text-sm font-semibold">REVIEWS</h2>
               </div>
 
               <div className="flex-1 border-b border-gray-200 pb-4 pr-4">
