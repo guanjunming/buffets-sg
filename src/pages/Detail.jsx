e73c6578881e5f8e979a4f6362381f631a21eb13
eeexuanlyx <eeexuanlyx@gmail.com>
2024-11-18 11:47:52 +0800

    change details page

--- src/pages/Detail.jsx
+++ src/pages/Detail.jsx
@@ -42,9 +42,9 @@ const Detail = () => {
   const shortenInfo = restaurant.description.slice(0, maxLength);
 
   return (
-    <div className="m-auto mx-9 max-w-screen-xl text-left">
-      <div className="text-sm">
-        <div className="mx-auto max-w-4xl p-6">
+    <div className="m-auto mx-9 text-left">
+      <div>
+        <div className="mx-auto p-6">
           <div className="md:flex-column flex flex-col gap-6 rounded-lg border border-gray-200 bg-white p-6 shadow-sm">
             <div className="flex items-center justify-between border-b border-gray-200 pb-4 pr-4">
               <h2 className="text-2xl font-bold">{restaurant.name}</h2>
@@ -111,11 +111,11 @@ const Detail = () => {
         </div>
       </div>
 
-      <div className="text-sm">
-        <div className="mx-auto max-w-4xl p-6">
+      <div>
+        <div className="mx-auto p-6">
           <div className="flex flex-col gap-6 rounded-lg border border-gray-200 bg-white p-6 shadow-sm md:flex-row">
             <div className="flex-1 border-r border-gray-200 pr-4">
-              <h2 className="mb-2 text-sm font-semibold">ABOUT</h2>
+              <h2 className="mb-2 font-semibold">ABOUT</h2>
 
               <div className="my-4 mb-2 border-t border-gray-300 leading-relaxed">
                 <p className="mt-4">
@@ -137,15 +137,15 @@ const Detail = () => {
               </div>
             </div>
 
-            <div className="flex-1 border-r border-gray-200 px-4">
-              <h2 className="mb-2 text-sm font-semibold">DETAILS</h2>
+            <div className="flex-1">
+              <h2 className="mb-2 font-semibold">DETAILS</h2>
               <div className="my-4 border-t border-gray-300">
                 <div className="mt-4">
                   <strong>Opening Hours:</strong>
                 </div>
                 <br />
                 {restaurant.openingHours.split("/").map((hour, index) => (
-                  <p key={index}>{hour.trim()}</p> // Trim whitespace before rendering
+                  <p key={index}>{hour.trim()}</p>
                 ))}
               </div>
               <div className="mt-4">
@@ -161,70 +161,69 @@ const Detail = () => {
                   </span>
                 ))}
               </div>
-              <p className="mt-4">
+              <div className="mt-4">
                 <strong>Price Range:</strong> Adult: from $
                 {restaurant.adultPrice?.min}{" "}
                 {restaurant.adultPrice.max && ` - ${restaurant.adultPrice.max}`}
                 ++
-              </p>
-            </div>
-
-            <div className="flex-1 pl-4">
-              <h2 className="mb-2 text-sm font-semibold">RATINGS & REVIEWS</h2>
-              <div className="my-4 border-t border-gray-300">
-                <strong>
-                  <p className="text-text-base mt-4">{`${restaurant.averageRating}/5`}</p>
-                </strong>
               </div>
-              <div className="flex flex-col items-center justify-center gap-1">
-                <Rating
-                  value={restaurant.averageRating}
-                  precision={0.1}
-                  sx={{
-                    color: "rgb(30,64,175)",
-                    "& .MuiRating-icon": { color: "rgb(30,64,175)" },
-                  }}
-                  readOnly
-                />
-              </div>
-              <p className="mt-4">
-                Based on{" "}
-                <button
-                  onClick={goToReviews} // Scroll to the reviews section on click
-                  className="text-blue-500 underline hover:text-blue-700"
-                >
-                  {restaurant.reviewCount} reviews
-                </button>
-              </p>
-              <div className="relative h-40">
-                <a
-                  href={restaurant.website}
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  className="absolute bottom-0 mb-4 mr-4 text-blue-500 hover:text-blue-700"
-                >
-                  Visit Website
-                </a>
+              <div className="mt-4">
+                <strong>Address:</strong>
+                {restaurant.address}
               </div>
             </div>
           </div>
         </div>
 
-        <div ref={reviewsRef} className="relative mx-auto max-w-4xl p-6">
-          {/* <div  className="absolute -top-14"></div> */}
-          <div className="flex flex-col gap-6 rounded-lg border border-gray-200 bg-white p-6 shadow-sm">
-            <div className="flex items-center justify-between border-b border-gray-200 pb-4">
-              <h2 className="mb-2 text-sm font-semibold">REVIEWS</h2>
-
-              <Link
-                to={`/review/${id}`}
-                className="rounded-md bg-blue-900 px-4 py-3 text-white hover:bg-blue-800"
-              >
-                Write a review
-              </Link>
+        <div className="flex flex-col gap-6 rounded-lg border border-gray-200 bg-white p-6 shadow-sm md:flex-row">
+          <div className="flex-1 basis-1/4 self-stretch border-r border-gray-200 p-4">
+            <h2 className="mt-4 font-semibold">RATINGS</h2>
+            <div className="my-4 border-t border-gray-300">
+              <strong>
+                <p className="text-text-base mt-8">{`${restaurant.averageRating}/5`}</p>
+              </strong>
             </div>
+            <div className="flex flex-col gap-1">
+              <Rating
+                value={restaurant.averageRating}
+                precision={0.1}
+                sx={{
+                  color: "rgb(30,64,175)",
+                  "& .MuiRating-icon": { color: "rgb(30,64,175)" },
+                }}
+                readOnly
+              />
+            </div>
+            <br></br>
+            <p>Based on {restaurant.reviewCount} reviews</p>
 
-            <ReviewsSection id={id} reviews={restaurant.reviews} />
+            <div className="relative h-20">
+              <a
+                href={restaurant.website}
+                target="_blank"
+                rel="noopener noreferrer"
+                className="absolute bottom-0 mb-4 mr-4 text-blue-500 hover:text-blue-700"
+              >
+                Visit Website
+              </a>
+            </div>
+          </div>
+
+          <div className="flex-1 basis-3/4 self-stretch p-6">
+            <div className="flex flex-col gap-6">
+              <div className="flex items-center justify-between border-b border-gray-200">
+                <h2 className="font-semibold">REVIEWS</h2>
+                <div className="mt-4 border-t border-gray-300"></div>
+                <Link
+                  to={`/review/${id}`}
+                  className="rounded-md bg-blue-900 px-4 py-3 text-white hover:bg-blue-800"
+                >
+                  Write a review
+                </Link>
+              </div>
+
+              <ReviewsSection id={id} reviews={restaurant.reviews} />
+            </div>
           </div>
         </div>
       </div>
