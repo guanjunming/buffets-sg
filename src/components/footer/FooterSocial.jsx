10a6823bc87aa6b2edc8358e8c62d200002348cb
KevinPramudyanto <kevinpramudyanto92@gmail.com>
2024-11-20 17:09:10 +0800

    Update footer

--- src/components/footer/FooterSocial.jsx
+++ src/components/footer/FooterSocial.jsx
@@ -1,24 +1,12 @@
-import {
-  FaFacebook,
-  FaYoutube,
-  FaInstagram,
-  FaTwitter,
-  FaLinkedin,
-  FaTelegram,
-  FaTiktok,
-} from "react-icons/fa";
+import { FaGithub } from "react-icons/fa";
 
 const FooterSocial = () => {
   return (
-    <div className="flex items-center justify-between gap-2 md:gap-5">
-      <div className="hidden md:block">Follow Us</div>
-      <FaFacebook size={20} />
-      <FaYoutube size={20} />
-      <FaInstagram size={20} />
-      <FaTwitter size={20} />
-      <FaLinkedin size={20} />
-      <FaTelegram size={20} />
-      <FaTiktok size={20} />
+    <div className="flex items-center justify-between gap-2 sm:gap-5">
+      <div>Follow Us</div>
+      <a href="https://github.com/guanjunming/buffets-sg" target="_blank">
+        <FaGithub size={20} />
+      </a>
     </div>
   );
 };
--- src/pages/Footer.jsx
+++ src/pages/Footer.jsx
@@ -2,7 +2,7 @@ import FooterSocial from "../components/footer/FooterSocial";
 
 const Footer = () => {
   return (
-    <div className="mx-5 mt-5 flex flex-col-reverse items-center justify-between gap-5 border-t border-neutral-500 p-5 md:flex-row">
+    <div className="mx-5 mt-5 flex flex-row items-center justify-between gap-5 border-t border-neutral-500 p-5 text-center">
       <div>Copyright © {new Date().getFullYear()}</div>
       <FooterSocial />
     </div>
