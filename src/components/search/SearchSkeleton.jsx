8b8a616196cc294395ce60dd0ac1b6860a5a13dd
KevinPramudyanto <kevinpramudyanto92@gmail.com>
2024-11-20 17:23:34 +0800

    Minor css update for SearchSkeleton

--- src/components/search/SearchSkeleton.jsx
+++ src/components/search/SearchSkeleton.jsx
@@ -2,7 +2,7 @@ import { Skeleton } from "@mui/material";
 
 const SearchSkeleton = () => {
   return (
-    <div className="overflow-hidden rounded-3xl border-y border-l border-r border-neutral-500 p-0 lg:flex lg:h-auto lg:w-full lg:flex-row lg:items-center lg:justify-start lg:gap-5 lg:rounded-none lg:border-y-0 lg:border-r-0 lg:bg-neutral-100 lg:p-2">
+    <div className="overflow-hidden rounded-3xl border border-neutral-500 p-0 lg:flex lg:h-auto lg:w-full lg:flex-row lg:items-center lg:justify-start lg:gap-5 lg:rounded-none lg:border-0 lg:bg-neutral-100 lg:p-2">
       <Skeleton
         variant="rectangular"
         className="mb-8 border-b border-neutral-500 pt-[56.25%] lg:mb-0 lg:w-1/2 lg:rounded-3xl lg:border lg:pt-[28.125%]"
